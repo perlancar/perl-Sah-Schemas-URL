1b4023f202194877772830b6fea4a1be138094b5
perlancar (@pc-office) <perlancar@gmail.com>
2016-07-22 19:39:53 +0700

    Initial commit

---
+++ lib/Sah/Schema/url.pm
@@ -0,0 +1,12 @@
+package Sah::Schema::url;
+
+# DATE
+# VERSION
+
+our $schema = ['str', {
+    summary => 'URL string',
+    min_len => 1,
+}, {}];
+
+1;
+# ABSTRACT:
---
+++ lib/Sah/Schemas/URL.pm
@@ -0,0 +1,7 @@
+package Sah::Schemas::URL;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Sah schemas related to URL
